e57785b6a1bb471e886afe8f229cb409a9d29454
dfe0990ngc <dfe0990ngc@gmail.com>
2024-03-19 08:01:44 +0800

    1. Updated app service provider code.

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\App;
 use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 
@@ -20,7 +21,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        if($this->app->environment('production')){
+        if(App::isProduction()){
             URL::forceScheme('https');
         }
     }
