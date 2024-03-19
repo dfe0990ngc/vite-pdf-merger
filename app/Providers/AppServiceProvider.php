6462740bd63e0e898d9669b39dae9e1e8b03c88b
dfe0990ngc <dfe0990ngc@gmail.com>
2024-03-19 18:42:34 +0800

    Updated db path

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -23,6 +23,15 @@ class AppServiceProvider extends ServiceProvider
     {
         if(App::isProduction()){
             URL::forceScheme('https');
+
+            // Ensure directories are writable by the web server
+            if (!is_writable('/var/task/user/public/uploads')) {
+                chmod('/var/task/user/public/uploads', 0755);
+            }
+
+            if (!is_writable('/var/task/user/public/db/database.sqlite')) {
+                chmod('/var/task/user/public/db/database.sqlite', 0755);
+            }
         }
     }
 }
