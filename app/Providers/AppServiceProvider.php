f7f08a1bea8a5bd6d566df2b5899abcff6482d14
dfe0990ngc <dfe0990ngc@gmail.com>
2024-04-02 08:24:44 +0800

    1. Fix firebase cred

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -24,5 +24,24 @@ class AppServiceProvider extends ServiceProvider
         if(App::isProduction()){
             URL::forceScheme('https');
         }
+
+        $myFile2 = "/tmp/firebase_credentials.json";
+        $myFileLink2 = fopen($myFile2, 'w+');
+        $newContents = json_encode(array(
+                'type' => env('FC_TYPE',''),
+                'project_id' => env('FC_PROJECT_ID',''),
+                'private_key_id' => env('FC_PRIVATE_KEY_ID',''),
+                'private_key' => env('FC_PRIVATE_KEY',''),
+                'client_email' => env('FC_CLIENT_EMAIL',''),
+                'client_id' => env('FC_CLIENT_ID',''),
+                'auth_uri' => env('FC_AUTH_URI',''),
+                'token_uri' => env('FC_TOKEN_URI',''),
+                'auth_provider_x509_cert_url' => env('FC_AUTH_PROVIDER_X509_CERT_URL',''),
+                'client_x509_cert_url' => env('FC_CLIENT_X509_CERT_URL',''),
+                'universe_domain' => env('FC_UNIVERSE_DOMAIN',''),
+            )
+        );
+        fwrite($myFileLink2, $newContents);
+        fclose($myFileLink2);
     }
 }
--- config/firebase.php
+++ config/firebase.php
@@ -50,22 +50,22 @@ return [
              *
              */
 
-            // 'credentials' => env('FIREBASE_CREDENTIALS', env('GOOGLE_APPLICATION_CREDENTIALS')),
+            'credentials' => env('FIREBASE_CREDENTIALS', env('GOOGLE_APPLICATION_CREDENTIALS')),
             
-            'credentials' => json_encode(array(
-                    'type' => env('FC_TYPE',''),
-                    'project_id' => env('FC_PROJECT_ID',''),
-                    'private_key_id' => env('FC_PRIVATE_KEY_ID',''),
-                    'private_key' => env('FC_PRIVATE_KEY',''),
-                    'client_email' => env('FC_CLIENT_EMAIL',''),
-                    'client_id' => env('FC_CLIENT_ID',''),
-                    'auth_uri' => env('FC_AUTH_URI',''),
-                    'token_uri' => env('FC_TOKEN_URI',''),
-                    'auth_provider_x509_cert_url' => env('FC_AUTH_PROVIDER_X509_CERT_URL',''),
-                    'client_x509_cert_url' => env('FC_CLIENT_X509_CERT_URL',''),
-                    'universe_domain' => env('FC_UNIVERSE_DOMAIN',''),
-                )
-            ),
+            // 'credentials' => json_encode(array(
+            //         'type' => env('FC_TYPE',''),
+            //         'project_id' => env('FC_PROJECT_ID',''),
+            //         'private_key_id' => env('FC_PRIVATE_KEY_ID',''),
+            //         'private_key' => env('FC_PRIVATE_KEY',''),
+            //         'client_email' => env('FC_CLIENT_EMAIL',''),
+            //         'client_id' => env('FC_CLIENT_ID',''),
+            //         'auth_uri' => env('FC_AUTH_URI',''),
+            //         'token_uri' => env('FC_TOKEN_URI',''),
+            //         'auth_provider_x509_cert_url' => env('FC_AUTH_PROVIDER_X509_CERT_URL',''),
+            //         'client_x509_cert_url' => env('FC_CLIENT_X509_CERT_URL',''),
+            //         'universe_domain' => env('FC_UNIVERSE_DOMAIN',''),
+            //     )
+            // ),
 
             /*
              * ------------------------------------------------------------------------
